20577a3c5a38bcd5754cd9a041b34b2b09df8cd9
aconanlai <aconanlai@gmail.com>
2017-02-17 13:57:12 -0500

    gooey

--- src/Components/TimeLineTest.js
+++ src/Components/TimeLineTest.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Waypoint from 'react-waypoint';
+
 require('smoothscroll-polyfill').polyfill();
+
+const YearDisplay = styled.span`
+  position: fixed;
+  text-align: center;
+  top: 0;
+  font-size: 80px;
+`;
 
 const Year2016 = styled.div`
   width: 100%;
-  height: 200vh;
+  height: 150vh;
+  padding-top: 75vh;
   background: yellow;
 `;
 
 const Year2015 = styled.div`
   width: 100%;
-  height: 200vh;
+  height: 150vh;
+  padding-top: 75vh;
   background: pink;
 `;
 
 const Year2014 = styled.div`
   width: 100%;
-  height: 200vh;
+  height: 150vh;
+  padding-top: 75vh;
   background: coral;
 `;
 
 const Year2013 = styled.div`
   width: 100%;
-  height: 200vh;
+  height: 150vh;
+  padding-top: 75vh;
   background: limegreen;
 `;
 
@@ -36,9 +49,11 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: '',
+      year: '',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleWaypointEnter = this.handleWaypointEnter.bind(this);
+    this.handleWaypointLeave = this.handleWaypointLeave.bind(this);
   }
 
   componentDidMount() {
@@ -60,37 +75,54 @@ class Home extends Component {
     document.querySelector(id).scrollIntoView({
       behavior: 'smooth',
     });
-    this.setState({ value: year });
+    this.setState({ year });
+  }
+
+  handleWaypointEnter(year) {
+    this.setState({ year });
+  }
+
+  handleWaypointLeave(year) {
+    console.log('leaving' + year);
   }
 
   render() {
     return (
       <div>
         <Select value={this.state.value} onChange={this.handleChange}>
-            <option value="2016">2016</option>
-            <option value="2015">2016</option>
-            <option value="2014">2014</option>
-            <option value="2013">2013</option>
-          </Select>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+          <option value="2014">2014</option>
+          <option value="2013">2013</option>
+        </Select>
+        <YearDisplay>{this.state.year}</YearDisplay>
         <Year2016>
-          <div id="year2016">
-            2016
+          <Waypoint onEnter={() => this.handleWaypointEnter('2016')}>
+            <div style={{ 'height': '50%', 'border': '1px solid black' }} id="year2016">
+              2016
           </div>
+          </Waypoint>
         </Year2016>
         <Year2015>
-          <div id="year2015">
+          <Waypoint onEnter={() => this.handleWaypointEnter('2015')}>
+            <div style={{ 'height': '50%', 'border': '1px solid black' }} id="year2015">
             2015
             </div>
+          </Waypoint>
         </Year2015>
         <Year2014>
-          <div id="year2014">
-            2014
+          <Waypoint onEnter={() => this.handleWaypointEnter('2014')}>
+            <div style={{ 'height': '50%', 'border': '1px solid black' }} id="year2014">
+              2014
             </div>
+          </Waypoint>
         </Year2014>
         <Year2013>
-          <div id="year2013">
-            2013
+          <Waypoint onEnter={() => this.handleWaypointEnter('2013')}>
+            <div style={{ 'height': '50%', 'border': '1px solid black' }} id="year2013">
+              2013
             </div>
+          </Waypoint>
         </Year2013>
       </div>
     );
